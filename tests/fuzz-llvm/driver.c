4524df464ef79595dfc7b22a1098f954aae6569b
Boris Kolpackov <boris@codesynthesis.com>
2019-12-05 17:07:27 +0200

    Fix stream mode handling bug in fuzz-llvm test

--- tests/fuzz-llvm/driver.c
+++ tests/fuzz-llvm/driver.c
@@ -15,12 +15,20 @@ parse(const void *data, size_t size, bool stream)
   json_open_buffer(json, data, size);
   json_set_streaming(json, stream);
 
-  while (t != JSON_ERROR && (t = json_next(json)) != JSON_DONE)
+  do
   {
+    t = json_next(json);
+
+    if (stream && t == JSON_DONE)
+    {
+      json_reset(json);
+      t = json_next(json); /* JSON_DONE if that was the last object. */
+    }
+
     /* Let's get a warning if any new values are added. */
     switch (t)
     {
-    case JSON_DONE:   assert(false);               break;
+    case JSON_DONE:                                break;
     case JSON_ERROR:  json_get_error(json);        break;
     case JSON_STRING: json_get_string(json, NULL); break;
     case JSON_NUMBER: json_get_number(json);       break;
@@ -30,9 +38,10 @@ parse(const void *data, size_t size, bool stream)
     case JSON_OBJECT:
     case JSON_OBJECT_END:
     case JSON_ARRAY:
-    case JSON_ARRAY_END: break;
+    case JSON_ARRAY_END:                           break;
     }
   }
+  while (t != JSON_DONE && t != JSON_ERROR);
 
   json_close(json);
 
